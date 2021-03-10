6bdfebbcf59e6ff9d976a795f2d19bef6801ad38
yunwisdom <qq695926920@163.com>
2021-03-10 17:32:02 +0800

    init

--- app/schedule/synctask.js
+++ app/schedule/synctask.js
@@ -7,8 +7,8 @@ class SyncTask extends Subscription {
     // 通过 schedule 属性来设置定时任务的执行间隔等配置
     static get schedule() {
         return {
-            cron: '*/5 * * * * *',
-            //interval: '10s', // 1 分钟间隔
+            //cron: '* * * * * *',
+            interval: '1m', // 1 分钟间隔
             type: 'worker', // 指定所有的 worker 都需要执行
         };
     }
