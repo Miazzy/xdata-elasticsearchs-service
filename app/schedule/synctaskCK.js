197dd9c912c3d14b0268fb657e90aaa99302dc8a
yunwisdom <qq695926920@163.com>
2021-03-16 15:39:27 +0800

    init

--- app/schedule/synctaskCK.js
+++ app/schedule/synctaskCK.js
@@ -7,7 +7,7 @@ class SyncTaskCK extends Subscription {
     static get schedule() {
         return {
             //cron: '* * * * * *',
-            interval: '1s', // 1 分钟间隔
+            interval: '5s', // 1 分钟间隔
             type: 'worker', // 指定所有的 worker 都需要执行
         };
     }
@@ -15,7 +15,7 @@ class SyncTaskCK extends Subscription {
     // subscribe 是真正定时任务执行时被运行的函数
     async subscribe(response, taskName = 'all', tasklist = [1, 2, 3, 4]) {
         response = await this.ctx.service.syncservice.doCkTask(taskName);
-        Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 100);
+        Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, 1000);
     }
 
 }
